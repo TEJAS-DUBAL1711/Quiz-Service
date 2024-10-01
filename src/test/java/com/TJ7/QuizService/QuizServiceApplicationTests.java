6de54bae38f8d010911e4174c871efdfedbd57c6
TEJAS-DUBAL1711 <tejasdubal2002@gmail.com>
2024-10-01 15:15:08 +0530

    initial commit

---
+++ src/test/java/com/TJ7/QuizService/QuizServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TJ7.QuizService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
